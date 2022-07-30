ed8329a335ac3dfac30e9557e04a058932c9587d
Outuby <outubyyt@gmail.com>
2022-07-30 18:55:10 +0200

    tod

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+var sot = "website made by outuby"
+var ahg = "https://outuby.space !!"
+var sih = "outuby loves exeos <3"
+var fig = "check out https://outuby.space if you also want a website like this"
+var t69 = "dm outuby#7969 if you want a personal website"
+var rko = "sub outuby on youtube"
+
+
+function t1() {
+	console.log(sot);
+ }
+ setInterval(t2, 1400);
+
+ function t2() {
+	console.log(ahg);
+ }
+ setInterval(t3, 1650);
+
+ function t3() {
+	console.log(sih);
+ }
+ setInterval(t4, 1551);
+
+ function t4() {
+	console.log(fig)
+ }
+ setInterval(t5, 1701);
+
+ function t5() {
+	console.log(t69)
+ }
+ setInterval(t6, 1621);
+
+ function t6() {
+	console.log(rko)
+ }
+ setInterval(t1, 1556);
+
+//  function t7() {
+// 	console.log(sse)
+//  }
+//  setInterval(t8, 1469);
+
+//  function t8() {
+// 	console.log(sex)
+//  }
+//  setInterval(t9, 1520);
+
+//  function t9() {
+// 	console.log(auxy);
+//  }
+//  setInterval(t1, 1443);
+
